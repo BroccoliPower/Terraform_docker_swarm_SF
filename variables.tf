aa193875b7f591d0524f384877d03edc5cce05f9
BroccoliPower <talyalev.kirill@gmail.com>
2024-05-05 18:33:34 +0300

    1

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "folder_id" {
+  description = "Default folder ID in yandex cloud"
+  type        = string
+  default     = "b1gpsodt75s8a25f1uq1"
+}
+
+variable "cloud_id" {
+  description = "Default folder ID in yandex cloud"
+  type        = string
+  default     = "b1gk57cnjqnl7dbk42or"
+}
